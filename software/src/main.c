6ded7aa18eb60539af1cedb905c1c4c149d68cb6
Francois Killeen <fkilleen@dashboard.wm.etsmtl.ca>
2013-10-26 14:45:39 -0400

    "test"

--- software/src/main.c
+++ software/src/main.c
@@ -8,10 +8,6 @@
 #include "bms.h"
 
 // Declarations des modules
-bms_t bms;
-battery_t cells[6];
-ad7280a_t ad72;
-acs_t acs;
 
 //Cell Monitoring Thread
 static WORKING_AREA(cell_monitor_wa, 128);
@@ -48,11 +44,27 @@ int main(int argc, char *argv[]) {
 //  // Init OS
   halInit();
   chSysInit();
-//
-//  // Init BMS
-//  init_bms(&bms);
-//  init_ad7280a(&ad72);
-//  battery_init(cells);
+
+
+  bms_t bms;
+  battery_t cells[6];
+  ad7280a_t ad72;
+  acs_t acs;
+  // Init BMS
+  init_bms(&bms);
+  init_ad7280a(&ad72);
+  battery_init(cells);
+
+  // Test Variables
+  ad7280a_packet_t PT;
+  uint32_t t1;
+  uint32_t t2;
+  uint32_t t3;
+  uint32_t t4;
+  uint32_t t5;
+  uint32_t t6;
+  uint8_t  crc;
+
 //
 //  // CellMonitor Thread
 //  chThdCreateStatic(cell_monitor_wa, sizeof(cell_monitor_wa),
@@ -64,11 +76,82 @@ int main(int argc, char *argv[]) {
 
   // Infinite loop
   while (true) {
- //   chThdSleepMilliseconds(133);
-//    battery_check_voltage(&ad72,cells);
-//    chThdSleepMilliseconds(133);
+
+
+
+
+      t1 = ad7280a_read_cell(1, &ad72);
+      PT.packed = ad72.rxbuf;
+      t2 = ad7280a_read_cell(2, &ad72);
+      PT.packed = ad72.rxbuf;
+      t3 = ad7280a_read_cell(3, &ad72);
+      PT.packed = ad72.rxbuf;
+      t4 = ad7280a_read_cell(4, &ad72);
+      PT.packed = ad72.rxbuf;
+      t5 = ad7280a_read_cell(5, &ad72);
+      PT.packed = ad72.rxbuf;
+      t6 = ad7280a_read_cell(6, &ad72);
+      PT.packed = ad72.rxbuf;
+
+
+
+
+
+
+
+
+
+
+//    // DEBUG TIME
+//
+//    (ad72.txbuf) = 0x038011CA;
+//    spi_exchange(&ad72);
+//    (ad72.txbuf) = 0x01A0131A;
+//    spi_exchange(&ad72);
+//    (ad72.txbuf) = 0x03A0546A;
+//    spi_exchange(&ad72);
+//
+//    // 5.Initiate conversions through the falling edge of CNVST.
+//    palClearPad(GPIOB,0);
+//
+//    // 5.1 Allow sufficient time for all conversions to be completed
+//    chThdSleepMilliseconds(ad72.delay_ms);
+//
+//    // 5.2 Latch the CNVST back to one
+//    palSetPad(GPIOB,0);
+//
+//    (ad72.txbuf) = 0xF800030A;
+//    spi_exchange(&ad72);
+//    PT.packed = ad72.rxbuf;
+//    t1 = PT.r_conversion.conversion_data;
+//
+//    (ad72.txbuf) = 0xF800030A;
+//    spi_exchange(&ad72);
+//    PT.packed = ad72.rxbuf;
+//    t2 = PT.r_conversion.conversion_data;
+//
+//    (ad72.txbuf) = 0xF800030A;
+//    spi_exchange(&ad72);
+//    PT.packed = ad72.rxbuf;
+//    t3 = PT.r_conversion.conversion_data;
+//
+//    (ad72.txbuf) = 0xF800030A;
+//    spi_exchange(&ad72);
+//    PT.packed = ad72.rxbuf;
+//    t4 = PT.r_conversion.conversion_data;
+//
+//    (ad72.txbuf) = 0xF800030A;
+//    spi_exchange(&ad72);
+//    PT.packed = ad72.rxbuf;
+//    t5 = PT.r_conversion.conversion_data;
+//
+//    (ad72.txbuf) = 0xF800030A;
+//    spi_exchange(&ad72);
+//    PT.packed = ad72.rxbuf;
+//    t6 = PT.r_conversion.conversion_data;
 
   }
+
     return 0;
 }
 
